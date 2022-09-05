92aad3476bfcb653131fd3e462efe88d1db9d13f
elevyg <elevyg91@gmail.com>
2022-09-05 09:27:30 -0400

    session added to trpc

--- packages/api/src/createContext.ts
+++ packages/api/src/createContext.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
+import { getSession } from '@auth0/nextjs-auth0';
 
 export const prisma = new PrismaClient({
   log:
@@ -17,11 +18,13 @@ export const createContext = async ({
   req,
   res,
 }: trpcNext.CreateNextContextOptions) => {
+  const session = req && res && getSession(req, res);
   // for API-response caching see https://trpc.io/docs/caching
   return {
     req,
     res,
     prisma,
+    session,
   };
 };
 
