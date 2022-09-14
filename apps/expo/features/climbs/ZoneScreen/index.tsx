08de3df7a49971651a767595b2d9080abc79f190
elevyg <elevyg91@gmail.com>
2022-09-14 07:57:43 -0300

    app: zone screen created

--- apps/expo/features/climbs/ZoneScreen/index.tsx
+++ apps/expo/features/climbs/ZoneScreen/index.tsx
@@ -1,17 +1,70 @@
-import { Box, Screen, Text } from '@andescalada/ui';
+import {
+  ActivityIndicator,
+  Box,
+  Pressable,
+  Screen,
+  Text,
+} from '@andescalada/ui';
+import { trpc } from '@andescalada/utils/trpc';
+import useRefresh from '@hooks/useRefresh';
 import {
   RootNavigationRoutes,
   RootNavigationScreenProps,
 } from '@navigation/AppNavigation/RootNavigation/types';
 import { FC } from 'react';
+import { Button, FlatList } from 'react-native';
 
 type Props = RootNavigationScreenProps<RootNavigationRoutes.Zone>;
 
-const ZoneScreen: FC<Props> = ({ route }) => {
+const ZoneScreen: FC<Props> = ({ route, navigation }) => {
+  const {} = trpc.useMutation(['sectors.add']);
+  const { data, refetch, isFetching, isLoading } = trpc.useQuery([
+    'sectors.all',
+  ]);
+  const refresh = useRefresh(refetch, isFetching);
+  if (isLoading)
+    return (
+      <Screen justifyContent="center" alignItems="center">
+        <ActivityIndicator size={'large'} />
+      </Screen>
+    );
   return (
     <Screen padding="m">
-      <Box>
+      <Box
+        flexDirection={'row'}
+        alignItems="center"
+        justifyContent={'space-between'}
+      >
         <Text variant={'h1'}>{route.params.zoneName}</Text>
+        <Button title="Agregar" />
+      </Box>
+      <Box flex={1}>
+        <FlatList
+          data={data}
+          refreshControl={refresh}
+          contentContainerStyle={{ flex: 1 }}
+          ListEmptyComponent={() => (
+            <Box flex={1} justifyContent="center" alignItems="center">
+              <Text variant={'h3'}>Sin sectores</Text>
+            </Box>
+          )}
+          renderItem={({ item }) => (
+            <Pressable
+              backgroundColor="listItemBackground"
+              alignItems="stretch"
+              padding="m"
+              marginVertical={'s'}
+              onPress={() =>
+                navigation.navigate(RootNavigationRoutes.Zone, {
+                  zoneId: item.id,
+                  zoneName: item.name,
+                })
+              }
+            >
+              <Text variant="p1R">{item.name}</Text>
+            </Pressable>
+          )}
+        />
       </Box>
     </Screen>
   );
