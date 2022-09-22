7db6f4dc98f37c1434df545dd75f78e1d24532bf
elevyg <elevyg91@gmail.com>
2022-09-22 16:43:46 -0300

    correct app owner

--- apps/expo/utils/auth0/params.ts
+++ apps/expo/utils/auth0/params.ts
@@ -1,4 +1,4 @@
 import { AUTH0_DOMAIN } from "@env";
 import { Platform } from "react-native";
 
-export const nativeReturnUrl = `andescalada://${AUTH0_DOMAIN}/${Platform.OS}/@eyalll/andescalada-app/callback`;
+export const nativeReturnUrl = `andescalada://${AUTH0_DOMAIN}/${Platform.OS}/@andescalada/andescalada-app/callback`;
