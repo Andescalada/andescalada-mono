70be21f35f1906df8d0a0080567de56fe66ae92d
elevyg <elevyg91@gmail.com>
2022-10-08 13:29:43 -0300

    feat(app): expo dev client and mmkv

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -1,3 +1,5 @@
+import "expo-dev-client";
+
 import { darkTheme } from "@andescalada/ui/Theme/navigationTheme";
 import ThemeProvider from "@andescalada/ui/Theme/ThemeProvider";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
