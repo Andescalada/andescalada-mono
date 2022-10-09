1425e96c783598313e22fa7ec25afa3b054cf938
elevyg <elevyg91@gmail.com>
2022-10-09 15:14:47 -0300

    chore(app): version bumped

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -4,4 +4,10 @@ export default ({ config }) => ({
   currentFullName: `@${config.owner}/${config.slug}`,
   originalFullName: `@${config.owner}/${config.slug}`,
   owner: config.owner,
+  runtimeVersion: {
+    policy: "sdkVersion",
+  },
+  updates: {
+    url: "https://u.expo.dev/a034137d-75c2-4941-a3b0-003e7b6ff487",
+  },
 });
