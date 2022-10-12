0758009f7bd87d373e3a5f4e76a03efe4e6f628f
elevyg <elevyg91@gmail.com>
2022-10-12 17:58:57 -0300

    feat: different bundle names for variants

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -1,13 +1,79 @@
-export default ({ config }) => ({
-  ...config,
-  slug: config.slug,
-  currentFullName: `@${config.owner}/${config.slug}`,
-  originalFullName: `@${config.owner}/${config.slug}`,
-  owner: config.owner,
-  runtimeVersion: {
-    policy: "sdkVersion",
-  },
-  updates: {
-    url: "https://u.expo.dev/a034137d-75c2-4941-a3b0-003e7b6ff487",
-  },
-});
+export default ({ config }) => variantConfig(config);
+
+const variantConfig = (config) => {
+  if (process.env.APP_VARIANT === "development") {
+    return {
+      ...config,
+      name: "Andescalada Dev",
+      icon: "./assets/expoConfig/icon_ae_dev.png",
+      slug: config.slug,
+      owner: config.owner,
+      currentFullName: `@${config.owner}/${config.slug}`,
+      originalFullName: `@${config.owner}/${config.slug}`,
+      ios: {
+        ...config.ios,
+        bundleIdentifier: "com.andescalada-dev.app",
+      },
+      android: {
+        ...config.android,
+        package: "com.andescalada-dev.app",
+        adaptiveIcon: {
+          foregroundImage:
+            "./assets/expoConfig/android-foregroundImage-adaptiveIcon_dev.png",
+          backgroundImage:
+            "./assets/expoConfig/android-backgroundImage-adaptiveIcon_dev.png",
+        },
+      },
+      runtimeVersion: {
+        policy: "sdkVersion",
+      },
+      updates: {
+        url: "https://u.expo.dev/a034137d-75c2-4941-a3b0-003e7b6ff487",
+      },
+    };
+  }
+  if (process.env.APP_VARIANT === "preview") {
+    return {
+      ...config,
+      name: "Andescalada Preview",
+      icon: "./assets/expoConfig/icon_ae_preview.png",
+      slug: config.slug,
+      owner: config.owner,
+      currentFullName: `@${config.owner}/${config.slug}`,
+      originalFullName: `@${config.owner}/${config.slug}`,
+      ios: {
+        ...config.ios,
+        bundleIdentifier: "com.andescalada-preview.app",
+      },
+      android: {
+        ...config.android,
+        package: "com.andescalada-preview.app",
+        adaptiveIcon: {
+          foregroundImage:
+            "./assets/expoConfig/android-foregroundImage-adaptiveIcon_preview.png",
+          backgroundImage:
+            "./assets/expoConfig/android-backgroundImage-adaptiveIcon_preview.png",
+        },
+      },
+      runtimeVersion: {
+        policy: "sdkVersion",
+      },
+      updates: {
+        url: "https://u.expo.dev/a034137d-75c2-4941-a3b0-003e7b6ff487",
+      },
+    };
+  }
+  return {
+    ...config,
+    slug: config.slug,
+    owner: config.owner,
+    currentFullName: `@${config.owner}/${config.slug}`,
+    originalFullName: `@${config.owner}/${config.slug}`,
+    runtimeVersion: {
+      policy: "sdkVersion",
+    },
+    updates: {
+      url: "https://u.expo.dev/a034137d-75c2-4941-a3b0-003e7b6ff487",
+    },
+  };
+};
