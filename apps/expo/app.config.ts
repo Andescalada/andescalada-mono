86175f7c5363e4bdef617b26916b72bcaa9160b7
elevyg <elevyg91@gmail.com>
2022-10-19 17:32:02 -0300

    fix(app): config files

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -11,33 +11,7 @@ const variantConfig = (config: ConfigContext["config"]): ExpoConfig => {
   if (process.env.APP_VARIANT === "preview") {
     return previewConfig(config);
   }
-  return {
-    ...config,
-    name: "Andescalada",
-    slug: "andescalada-app",
-    owner: config.owner,
-    currentFullName: `@${config.owner}/${config.slug}`,
-    originalFullName: `@${config.owner}/${config.slug}`,
-    runtimeVersion: {
-      policy: "sdkVersion",
-    },
-    updates: {
-      url: "https://u.expo.dev/a034137d-75c2-4941-a3b0-003e7b6ff487",
-    },
-    extra: {
-      ...config.extra,
-      API_URL: "https://andescalada-mono.vercel.app",
-      UPSTASH_REDIS_REST_URL: "https://us1-rational-wahoo-38618.upstash.io",
-      UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
-      SENTRY_DEPLOY_ENV: "production",
-      AUTH0_DOMAIN: "andescalada.us.auth0.com",
-      AUTH0_CLIENT_ID: "HpNoZWmoc6zbp1mmDlhNPsgWRsbBwJ39",
-      AUTH0_AUDIENCE: "https://api.andescalada.org/",
-      CLOUDINARY_UPLOAD_PRESET: "andescalada-app",
-      CLOUDINARY_URL:
-        "https://api.cloudinary.com/v1_1/fundacion-andescalada/image/upload",
-    },
-  };
+  return productionConfig(config);
 };
 
 const previewConfig = (config: ConfigContext["config"]): ExpoConfig => ({
@@ -119,6 +93,34 @@ export const devConfig = (config: ConfigContext["config"]): ExpoConfig => ({
     AUTH0_CLIENT_ID: "8rRn5mILmCShT1wjCZ38wLR4Plopabk4",
     AUTH0_AUDIENCE: "https://api-dev.andescalada.org/",
     CLOUDINARY_UPLOAD_PRESET: "andescalada-app-dev",
+    CLOUDINARY_URL:
+      "https://api.cloudinary.com/v1_1/fundacion-andescalada/image/upload",
+  },
+});
+
+const productionConfig = (config: ConfigContext["config"]): ExpoConfig => ({
+  ...config,
+  name: "Andescalada",
+  slug: "andescalada-app",
+  owner: config.owner,
+  currentFullName: `@${config.owner}/${config.slug}`,
+  originalFullName: `@${config.owner}/${config.slug}`,
+  runtimeVersion: {
+    policy: "sdkVersion",
+  },
+  updates: {
+    url: "https://u.expo.dev/a034137d-75c2-4941-a3b0-003e7b6ff487",
+  },
+  extra: {
+    ...config.extra,
+    API_URL: "https://andescalada-mono.vercel.app",
+    UPSTASH_REDIS_REST_URL: "https://us1-rational-wahoo-38618.upstash.io",
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+    SENTRY_DEPLOY_ENV: "production",
+    AUTH0_DOMAIN: "andescalada.us.auth0.com",
+    AUTH0_CLIENT_ID: "HpNoZWmoc6zbp1mmDlhNPsgWRsbBwJ39",
+    AUTH0_AUDIENCE: "https://api.andescalada.org/",
+    CLOUDINARY_UPLOAD_PRESET: "andescalada-app",
     CLOUDINARY_URL:
       "https://api.cloudinary.com/v1_1/fundacion-andescalada/image/upload",
   },
