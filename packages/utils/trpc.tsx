6dce7ff40f6f5398d8a689aedf638419188d4189
elevyg <elevyg91@gmail.com>
2022-10-23 23:45:39 -0300

    chore(app): trpc updated

--- packages/utils/trpc.tsx
+++ packages/utils/trpc.tsx
@@ -1,4 +1,4 @@
 import type { AppRouter } from "@andescalada/api/src/routers/_app";
-import { createTRPCReact } from "@trpc/react";
+import { createTRPCReact } from "@trpc/react-query";
 
 export const trpc = createTRPCReact<AppRouter>();
