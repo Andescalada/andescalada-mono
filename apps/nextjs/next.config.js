f2d2cbd92f0efde07fc7344416d67a38a00e9ecd
elevyg <elevyg91@gmail.com>
2022-11-10 10:38:35 -0300

    fix(nextjs): upgrade typescript

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -1,7 +1,3 @@
-/**
- * @link https://nextjs.org/docs/api-reference/next.config.js/introduction
- * @type {import('next').NextConfig}
- */
 const nextConfig = {
   experimental: {
     externalDir: true,
