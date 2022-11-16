1b9af980a36d89cb425133c5c7ee75f00ec70de8
elevyg <elevyg91@gmail.com>
2022-11-16 10:19:23 -0300

    fix(app): weird drawing behavior on android

--- packages/climbs-drawer/SkiaRouteCanvas/SkiaRouteCanvas.tsx
+++ packages/climbs-drawer/SkiaRouteCanvas/SkiaRouteCanvas.tsx
@@ -1,13 +1,11 @@
 import {
   Canvas,
-  Circle,
-  Group,
   SkiaMutableValue,
   useMultiTouchHandler,
   useValue,
 } from "@shopify/react-native-skia";
 import { FC } from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, Platform } from "react-native";
 
 import { GestureHandler } from "../GestureHandler/GestureHandler";
 import { Picture } from "../SkiaPicture/SkiaPicture";
@@ -16,7 +14,6 @@ import useCacheImage from "../useCacheImage/useCacheImage";
 export const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } =
   Dimensions.get("window");
 
-const DEBUG = __DEV__ && true;
 interface Props {
   imageUrl: string;
   height: number;
@@ -36,26 +33,21 @@ const SkiaRouteCanvas: FC<Props> = ({
   children,
   coords,
 }) => {
-  const pointerX = useValue(0);
-  const pointerY = useValue(0);
-  const pointer2X = useValue(0);
-  const pointer2Y = useValue(0);
+  const ids = useValue<number[]>([]);
 
   const touchHandler = useMultiTouchHandler({
     onEnd: ({ x, y, id }) => {
+      console.log(ids.current);
       if (coords) {
+        console.log({ id });
         const isXOutOfBound = Math.abs(x) > width;
         const isYOutOfBound = Math.abs(y) > height;
 
-        if (id === 0 && DEBUG) {
-          pointerX.current = x;
-          pointerY.current = y;
-        }
-        if (id === 1 && DEBUG) {
-          pointer2X.current = x;
-          pointer2Y.current = y;
-        }
-        if (isXOutOfBound || isYOutOfBound || id > 0) {
+        if (
+          isXOutOfBound ||
+          isYOutOfBound ||
+          (Platform.OS === "android" && id > 0)
+        ) {
           return;
         }
         coords.current = { x, y };
@@ -76,12 +68,6 @@ const SkiaRouteCanvas: FC<Props> = ({
           onTouch={touchHandler}
         >
           <Picture height={height} width={width} image={image} />
-          {DEBUG && (
-            <Group>
-              <Circle r={5} cx={pointerX} cy={pointerY} color={"red"} />
-              <Circle r={5} cx={pointer2X} cy={pointer2Y} color={"blue"} />
-            </Group>
-          )}
           {children}
         </Canvas>
       </GestureHandler>
