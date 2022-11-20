b98165b32a39b3ed9bad9b1a4025b66b6cadc3b6
elevyg <elevyg91@gmail.com>
2022-11-20 16:51:17 -0300

    feat(app): delete un wanted images

--- apps/expo/features/offline/useOffline.ts
+++ apps/expo/features/offline/useOffline.ts
@@ -1,21 +1,14 @@
 import { AppRouter } from "@andescalada/api/src/routers/_app";
 import { trpc } from "@andescalada/utils/trpc";
-import { useAppDispatch, useAppSelector } from "@hooks/redux";
+import setAssetsToDb from "@features/offline/utils/setAssetsToDb";
+import setImagesToFileSystem from "@features/offline/utils/setImagesToFileSystem";
+import { useAppSelector } from "@hooks/redux";
 import useOfflineMode from "@hooks/useOfflineMode";
-import {
-  clearErrors,
-  setError,
-  setIsDownloading,
-  setProgress,
-} from "@store/offline";
 import { onlineManager } from "@tanstack/react-query";
 import { inferProcedureOutput } from "@trpc/server";
 import allSettled from "@utils/allSetled";
-import { getThumbnail, optimizedImage } from "@utils/cloudinary";
-import fileSystem from "@utils/FileSystem";
 import storage, { Storage } from "@utils/mmkv/storage";
 import offlineDb from "@utils/quick-sqlite";
-import client from "@utils/trpc/client";
 import { useCallback, useEffect } from "react";
 import { useIsConnected } from "react-native-offline";
 import { parse, stringify } from "superjson";
@@ -35,113 +28,6 @@ const useOffline = ({ fetchAssets = false }: Args = {}) => {
   const utils = trpc.useContext();
   const { isDownloading, progress, errors } = useAppSelector(
     (state) => state.offline,
-  );
-  const dispatch = useAppDispatch();
-
-  const setAssetsToDb = useCallback(
-    async (
-      assetsToDownload: ListToDownload["assetsToDownload"],
-      {
-        checkVersion = true,
-        forceUpdate = false,
-      }: {
-        checkVersion?: boolean;
-        forceUpdate?: boolean;
-      } = {
-        checkVersion: true,
-        forceUpdate: false,
-      },
-    ) => {
-      if (!assetsToDownload) return;
-
-      storage.set(Storage.DOWNLOADED_ASSETS, stringify(assetsToDownload));
-
-      dispatch(setProgress(0));
-      dispatch(clearErrors());
-      const db = offlineDb.open();
-
-      const filteredAssets = assetsToDownload.filter((asset) => {
-        const { params, router, procedure, version, zoneId } = asset;
-        const queryKey = stringify({ router, procedure, params });
-        const savedData = offlineDb.get(db, queryKey, zoneId);
-
-        return (
-          !savedData ||
-          (checkVersion && savedData.version < version) ||
-          forceUpdate
-        );
-      });
-
-      if (filteredAssets.length === 0) return;
-
-      const totalAssets = filteredAssets.length;
-
-      dispatch(setIsDownloading(true));
-      await filteredAssets.reduce(async (prevAsset, asset, index) => {
-        const { params, router, procedure, version, zoneId } = asset;
-        const queryKey = stringify({ router, procedure, params });
-
-        try {
-          //@ts-ignore
-          const selectedClient = client[router][procedure];
-          const savedData = offlineDb.get(db, queryKey, zoneId);
-
-          //@ts-ignore
-          const data = await selectedClient.query(params);
-
-          await offlineDb.setOrCreate(db, queryKey, zoneId, data, version);
-        } catch (error) {
-          let message = "Unknown Error";
-          if (error instanceof Error) message = error.message;
-          dispatch(setError({ [queryKey]: message }));
-        }
-        const r = (await prevAsset) + 1;
-        dispatch(setProgress((index + 1) / totalAssets));
-
-        return r;
-      }, Promise.resolve(0));
-
-      dispatch(setIsDownloading(false));
-
-      db.close();
-    },
-    [dispatch],
-  );
-
-  const setImagesToFileSystem = useCallback(
-    async (assetsToDownload: ListToDownload["imagesToDownload"]) => {
-      if (!assetsToDownload) return;
-
-      storage.set(Storage.DOWNLOADED_IMAGES, stringify(assetsToDownload));
-      assetsToDownload.forEach(async (asset) => {
-        const { publicId } = asset;
-        if (!publicId) return;
-        const thumbnail = getThumbnail(publicId);
-
-        const cachedThumbnail = async () =>
-          thumbnail &&
-          fileSystem.storeImage(
-            {
-              url: thumbnail.url,
-              uniqueId: thumbnail.uniqueId,
-            },
-            "permanent",
-          );
-
-        const mainImage = optimizedImage(publicId);
-        const cachedMainImage = async () =>
-          mainImage &&
-          fileSystem.storeImage(
-            {
-              url: mainImage.url,
-              uniqueId: mainImage.uniqueId,
-            },
-            "permanent",
-          );
-        allSettled([cachedThumbnail(), cachedMainImage()]);
-      });
-    },
-    [],
   );
 
   trpc.user.getDownloadedAssets.useQuery(undefined, {
@@ -196,7 +82,6 @@ const useOffline = ({ fetchAssets = false }: Args = {}) => {
     progress,
     errors,
     setAssetsToDb,
-    setImagesToFileSystem,
   };
 };
 
---
+++ apps/expo/features/offline/utils/setImagesToFileSystem.ts
@@ -0,0 +1,67 @@
+import { AppRouter } from "@andescalada/api/src/routers/_app";
+import { inferProcedureOutput } from "@trpc/server";
+import allSettled from "@utils/allSetled";
+import { getThumbnail, optimizedImage } from "@utils/cloudinary";
+import fileSystem from "@utils/FileSystem";
+import storage, { Storage } from "@utils/mmkv/storage";
+import { parse, stringify } from "superjson";
+
+type ListToDownload = inferProcedureOutput<
+  AppRouter["user"]["getDownloadedAssets"]
+>;
+
+const setImagesToFileSystem = async (
+  assetsToDownload: ListToDownload["imagesToDownload"],
+) => {
+  if (!assetsToDownload) return;
+
+  const savedImagesString = storage.getString(Storage.DOWNLOADED_IMAGES);
+
+  const savedImages = savedImagesString
+    ? parse<ListToDownload["imagesToDownload"]>(savedImagesString)
+    : [];
+
+  const imagesToDelete = savedImages.filter(
+    (image) => !assetsToDownload.find((i) => i.id === image.id),
+  );
+
+  await imagesToDelete.reduce(async (prevImage, image) => {
+    const { id } = image;
+    await fileSystem.deleteImage(id);
+    return prevImage;
+  }, Promise.resolve());
+
+  await assetsToDownload.reduce(async (prevAsset, asset) => {
+    const { publicId } = asset;
+    if (!publicId) return (await prevAsset) + 1;
+
+    const thumbnail = getThumbnail(publicId);
+
+    const cachedThumbnail = async () =>
+      thumbnail &&
+      fileSystem.storeImage(
+        {
+          url: thumbnail.url,
+          uniqueId: thumbnail.uniqueId,
+        },
+        "permanent",
+      );
+
+    const mainImage = optimizedImage(publicId);
+    const cachedMainImage = async () =>
+      mainImage &&
+      fileSystem.storeImage(
+        {
+          url: mainImage.url,
+          uniqueId: mainImage.uniqueId,
+        },
+        "permanent",
+      );
+    allSettled([cachedThumbnail(), cachedMainImage()]);
+    return (await prevAsset) + 1;
+  }, Promise.resolve(0));
+
+  storage.set(Storage.DOWNLOADED_IMAGES, stringify(assetsToDownload));
+};
+
+export default setImagesToFileSystem;
