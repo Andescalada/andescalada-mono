7f7c3cd2c3ae0ac3494124b3c1a2c19040f6c962
elevyg <elevyg91@gmail.com>
2022-12-02 14:55:02 -0300

    refactor(icons): transformed icons saved in components folder

--- packages/icons/map.ts
+++ packages/icons/map.ts
@@ -1,4 +1,4 @@
-const base = "./Icons/";
+const base = "./components/";
 
 export const Icons = {
   "no-camping-color": require(base + "no-camping-color").default,
--- packages/icons/transform.sh
+++ packages/icons/transform.sh
@@ -5,21 +5,21 @@ npx svgo -f svg
 echo "Done cleaning :broom:"
 
 echo "Transforming icons"
-rm -r Icons
-mkdir Icons
-npx @svgr/cli --native --icon --no-prettier --filename-case kebab --replace-attr-values "#000={props.fill}" --out-dir icons -- svg 
+rm -r components
+mkdir components
+npx @svgr/cli --native --icon --no-prettier --filename-case kebab --replace-attr-values "#000={props.fill}" --out-dir components -- svg 
 
 echo "Done transforming icons"
 
 rm map.ts
 
 printf "\e[1m\e[92mCreating the map...\e[0m\n"
-echo "const base = \"./Icons/\";" > map.ts
+echo "const base = \"./components/\";" > map.ts
 echo >> map.ts
 echo "export const Icons = {" >> map.ts
-for file in Icons/*.js
+for file in components/*.js
 do
-  if [ "$file" != "Icons/index.js" ]
+  if [ "$file" != "components/index.js" ]
   then
       base_name=$(basename ${file%.js})
       echo $base_name
