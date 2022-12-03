8232ff5a22f3b5d070fe139368066d2bd41e73c2
elevyg <elevyg91@gmail.com>
2022-12-03 15:48:49 -0300

    feat(api+db): multi-language

--- packages/api/src/createContext.ts
+++ packages/api/src/createContext.ts
@@ -2,8 +2,11 @@ import { PrismaClient, SoftDelete } from "@prisma/client";
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { Redis } from "@upstash/redis";
+import acceptLanguage from "accept-language";
 
 import verifyAndDecodeToken from "./utils/verifyAndDecodeToken";
+
+const FALLBACK_LANGUAGE = "es";
 
 export const prisma = new PrismaClient({
   log:
@@ -17,6 +20,7 @@ export const access = new Redis({
   token: process.env.UPSTASH_REDIS_REST_TOKEN || "",
 });
 
+acceptLanguage.languages(["es-Cl", "es-EN"]);
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
@@ -26,6 +30,13 @@ export const createContext = async ({
   res,
 }: trpcNext.CreateNextContextOptions) => {
   const { user, verified } = await verifyAndDecodeToken(req);
+
+  const language = {
+    language: acceptLanguage.get(req.headers["accept-language"]),
+    simplified:
+      acceptLanguage.get(req.headers["accept-language"])?.split("-")[0] ??
+      FALLBACK_LANGUAGE,
+  };
 
   if (user?.email && user?.auth0Id) {
     const userExist = await prisma.user.findUnique({
@@ -67,6 +78,7 @@ export const createContext = async ({
     user,
     access,
     verified,
+    language,
   };
 };
 
