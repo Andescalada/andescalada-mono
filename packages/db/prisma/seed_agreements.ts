56c6a5653c4929ff01c17a2b333e3f7516d9e09a
elevyg <elevyg91@gmail.com>
2022-12-03 16:38:47 -0300

    feat(db): classic agreements seed and fix in schema

---
+++ packages/db/prisma/seed_agreements.ts
@@ -0,0 +1,210 @@
+module.exports = {};
+
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const authorEmail = "elevy@andescalada.org";
+
+const agreements: Prisma.AgreementCreateInput[] = [
+  {
+    title: {
+      create: {
+        originalText: "Se permiten mascotas",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    description: {
+      create: {
+        originalText:
+          "Tus mascotas son bienvenidas en esta zona de escalada. Se responsable y respetuoso.",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    Author: { connect: { email: authorEmail } },
+    classic: "Pets",
+    icon: "dog",
+  },
+  {
+    title: {
+      create: {
+        originalText: "No se permiten mascotas",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    description: {
+      create: {
+        originalText:
+          "Lamentamos que tus mascotas no puedan acompañarte en esta zona de escalada.",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    Author: { connect: { email: authorEmail } },
+    classic: "Pets",
+    icon: "no-dog",
+  },
+  {
+    title: {
+      create: {
+        originalText: "No se permite acampar",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    description: {
+      create: {
+        originalText: "No esta permitido acampar en esta zona de escalada",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    Author: { connect: { email: authorEmail } },
+    classic: "Camping",
+    icon: "no-camping",
+  },
+  {
+    title: {
+      create: {
+        originalText: "Permitido acampar",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    description: {
+      create: {
+        originalText:
+          "Es posible acampar en esta zona de escalada respetando las reglas.",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    Author: { connect: { email: authorEmail } },
+    classic: "Camping",
+    icon: "camping",
+  },
+  {
+    title: {
+      create: {
+        originalText: "No hacer fuego",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    description: {
+      create: {
+        originalText: "No esta permitido hacer fuego en esta zona de escalada.",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    Author: { connect: { email: authorEmail } },
+    classic: "Fire",
+    icon: "no-fire",
+  },
+  {
+    title: {
+      create: {
+        originalText: "Permitido hacer fuego",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    description: {
+      create: {
+        originalText:
+          "Está permitido hacer fuego en zonas designadas, recuerda dejarlo bien apagado.",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    Author: { connect: { email: authorEmail } },
+    classic: "Fire",
+    icon: "fire",
+  },
+  {
+    title: {
+      create: {
+        originalText: "Pagado",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    description: {
+      create: {
+        originalText:
+          "Es necesario pagar para acceder a esta zona de escalada.",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    Author: { connect: { email: authorEmail } },
+    classic: "Payment",
+    icon: "paid-hands",
+  },
+  {
+    title: {
+      create: {
+        originalText: "Libre",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    description: {
+      create: {
+        originalText:
+          "No es necesario pagar para acceder a esta zona de escalada.",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    Author: { connect: { email: authorEmail } },
+    classic: "Payment",
+    icon: "shake-hands",
+  },
+  {
+    title: {
+      create: {
+        originalText: "Usa el baño habilitado",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    description: {
+      create: {
+        originalText:
+          "Existe un baño habilitado para que hagas tus necesidades.",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    Author: { connect: { email: authorEmail } },
+    classic: "Toilet",
+    icon: "toilet",
+  },
+  {
+    title: {
+      create: {
+        originalText: "No dejes rastro",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    description: {
+      create: {
+        originalText:
+          "No existe un baño habilitado, por favor no dejes rastro. Entierra o llévate tus caca.",
+        originalLang: { connect: { languageId: "es" } },
+      },
+    },
+    Author: { connect: { email: authorEmail } },
+    classic: "Toilet",
+    icon: "poop-bag",
+  },
+];
+
+async function main() {
+  console.log("Creating agreements...");
+
+  for (const agreement of agreements) {
+    await prisma.agreement.create({
+      data: agreement,
+    });
+  }
+
+  console.log("Agreements created!");
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
