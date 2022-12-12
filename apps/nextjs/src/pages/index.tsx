7cff1a54585e05dda823a5656f86f3f4c1148f68
elevyg <elevyg91@gmail.com>
2022-12-12 19:55:48 -0300

    chore(nextjs): force update

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 export default function IndexPage() {
   return (
     <div>
-      <h1>Api Andescalada v0.0.3-c</h1>
+      <h1>Api Andescalada v0.0.3-d</h1>
       <ReactQueryDevtools />
     </div>
   );
