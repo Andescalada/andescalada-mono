9ef3a72f9706c779bbb5a3a350630a45ef43af9a
elevyg <elevyg91@gmail.com>
2023-01-21 12:19:13 -0300

    refactor(app): rename component

--- apps/expo/features/climbs/ZoneScreen/ZoneScreen.tsx
+++ apps/expo/features/climbs/ZoneScreen/ZoneScreen.tsx
@@ -7,7 +7,7 @@ import {
   ClimbsNavigationRoutes,
   ClimbsNavigationScreenProps,
 } from "@features/climbs/Navigation/types";
-import SectorsGateway from "@features/climbs/ZoneScreen/NoSectors";
+import SectorsGateway from "@features/climbs/ZoneScreen/SectosGateway";
 import ZoneHeader from "@features/climbs/ZoneScreen/ZoneHeader";
 import ZoneItem from "@features/climbs/ZoneScreen/ZoneItem";
 import useOfflineMode from "@hooks/useOfflineMode";
