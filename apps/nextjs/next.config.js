558ce54ceee21f7b039963259ba77c971aa33bb2
elevyg <elevyg91@gmail.com>
2023-01-22 11:41:44 -0300

    fix(nextjs): next js config

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -1,7 +1,17 @@
-const nextConfig = {
-  experimental: {
-    externalDir: true,
-  },
+// @ts-check
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: [
+    "@andescalada/api",
+    "@andescalada/utils",
+    "@andescalada/db",
+  ],
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: !!process.env.CI },
+  typescript: { ignoreBuildErrors: !!process.env.CI },
 };
 
-module.exports = nextConfig;
+module.exports = config;
