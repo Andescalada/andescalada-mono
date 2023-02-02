f2fe5fc24fc70fca54c406d323d25724bc149644
elevyg <elevyg91@gmail.com>
2023-02-02 20:15:33 -0300

    chore(nextjs): add description and title

--- apps/nextjs/src/pages/layout.tsx
+++ apps/nextjs/src/pages/layout.tsx
@@ -1,4 +1,5 @@
 import { Rubik } from "@next/font/google";
+import Head from "next/head";
 import { FC, PropsWithChildren } from "react";
 
 const rubik = Rubik({
@@ -8,6 +9,14 @@ const rubik = Rubik({
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => (
   <>
+    <Head>
+      <title>Andescalada App</title>
+      <meta
+        name="description"
+        content="Andescalada, la app de la Fundación Andescalada para gestionar la información y a la comunidad escaladora."
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <main className={`${rubik.variable}`}>{children}</main>
   </>
 );
