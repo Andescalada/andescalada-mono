09d9ec6006a1b5b052c5867299b7ea8971dabcf9
elevyg <elevyg91@gmail.com>
2023-02-08 23:15:58 -0300

    feat(nextjs): AASA

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -37,6 +37,14 @@ const config = {
       },
     ],
   },
+  headers: async () => {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [{ key: "content-type", value: "application/json" }],
+      },
+    ];
+  },
   experimental: {
     swcPlugins: [
       [
