f946bd1b6373627b1432bba4e8c7719ea94ec7f9
elevyg <elevyg91@gmail.com>
2023-02-10 09:43:59 -0300

    feat(nextjs): privacy policy

---
+++ apps/nextjs/src/pages/politica-de-privacidad.tsx
@@ -0,0 +1,325 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <section className="m-20 flex flex-col space-y-4">
+      <p>
+        <span>
+          Pol&iacute;tica de Privacidad de Aplicaci&oacute;n Andescalada
+        </span>
+      </p>
+      <p>
+        <span>
+          Fecha de la &uacute;ltima revisi&oacute;n: 20 de febrero de 2023
+        </span>
+      </p>
+      <p>
+        <span></span>
+      </p>
+      <h1>
+        <span>Pol&iacute;tica de Privacidad</span>
+      </h1>
+      <p>
+        <span>
+          La presente Pol&iacute;tica de Privacidad se aplica a la
+          utilizaci&oacute;n de la aplicaci&oacute;n m&oacute;vil Andescalada,
+          propiedad de Fundaci&oacute;n Andescalada, RUT 65.204.425-5.{" "}
+        </span>
+      </p>
+      <p>
+        <span></span>
+      </p>
+      <h2>
+        <span>
+          I. &iquest;Qu&eacute; tipo de informaci&oacute;n recopilamos?
+        </span>
+      </h2>
+      <p>
+        <span>
+          A continuaci&oacute;n listamos el detalle de la informaci&oacute;n que
+          recopilamos y almacenamos a trav&eacute;s de la aplicaci&oacute;n
+          m&oacute;vil Andescalada:
+        </span>
+      </p>
+      <ul>
+        <li>
+          <span>Informaci&oacute;n personal que nos proporcionas: </span>
+          <span>
+            Tu correo electrónico es el &uacute;nico dato que te pediremos de
+            forma obligatoria, ya que ser&aacute; tu identificador en la
+            aplicaci&oacute;n m&oacute;vil. Adem&aacute;s, opcionalmente
+            podr&aacute;s actualizar tu nombre, imagen de perfil,
+            ubicaci&oacute;n y biograf&iacute;a.
+          </span>
+        </li>
+        <li>
+          <span>Informaci&oacute;n de coaliciones: </span>
+          <span>
+            Coaliciones a las que perteneces e iniciativas a las que te sumas.
+          </span>
+        </li>
+        <li>
+          <span>Informaci&oacute;n de zonas y rutas de escalada:</span>
+          <span>
+            &nbsp;Zonas de escalada descargadas y vistas recientemente, rutas
+            guardadas, marcadas como hechas y evaluaci&oacute;n de estas.
+            Tambi&eacute;n recopilaremos informaci&oacute;n de las rutas que has
+            equipado o
+          </span>
+          <span>de las rutas a las que has aportado</span>
+          <span>
+            , ya sea cargando esta en la aplicaci&oacute;n o actualizando
+            informaci&oacute;n de esta y su estado.
+          </span>
+        </li>
+        <li>
+          <span>Informaci&oacute;n de b&uacute;squeda: </span>
+          <span>
+            &Uacute;ltimas b&uacute;squedas realizadas en la aplicaci&oacute;n
+            m&oacute;vil, ya sean zonas, paredes, rutas, coaliciones o
+            iniciativas.
+          </span>
+        </li>
+        <li>
+          <span>Ubicaci&oacute;n actual:</span>
+          <span>
+            &nbsp;Ubicaci&oacute;n desde d&oacute;nde accedes a la
+            aplicaci&oacute;n m&oacute;vil. La informaci&oacute;n relacionada
+            con la ubicaci&oacute;n puede basarse en factores como la
+            ubicaci&oacute;n exacta del dispositivo (si nos permitiste recopilar
+            esta informaci&oacute;n).
+          </span>
+        </li>
+        <li>
+          <span>Informaci&oacute;n de los dispositivos:</span>
+          <span>&nbsp;</span>
+          <span>
+            Atributos del dispositivo m&oacute;vil que utilizas, como sistema
+            operativo, versiones del hardware y software
+          </span>
+          <span>.</span>
+        </li>
+      </ul>
+      <p>
+        <span></span>
+      </p>
+      <h2>
+        <span>II. &iquest;C&oacute;mo usamos esta informaci&oacute;n?</span>
+      </h2>
+      <p>
+        <span>
+          Usamos la informaci&oacute;n que tenemos como se describe a
+          continuaci&oacute;n:
+        </span>
+      </p>
+      <ul>
+        <li>
+          <span className="font-bold my-2">
+            Proporcionamos, personalizamos y mejoramos la aplicaci&oacute;n:
+            <br />
+          </span>
+          <span>
+            Utilizamos la informaci&oacute;n que nos has proporcionado, para
+            mejorar tu experiencia en la aplicaci&oacute;n. Por ejemplo, la
+            informaci&oacute;n relacionada con la ubicaci&oacute;n o las rutas
+            de escalada realizadas nos permite personalizar el uso en la
+            aplicaci&oacute;n m&oacute;vil, a fin de que resulten m&aacute;s
+            relevantes para ti. La informaci&oacute;n de tu n&uacute;mero de
+            tel&eacute;fono solo se usa para validar tu autenticidad y para una
+            eventual comunicaci&oacute;n contigo. Los otros datos de tu perfil
+            que est&aacute;n p&uacute;blicos solo se muestran en esa
+            secci&oacute;n y los que son privados se guardan para que t&uacute;
+            mismo puedas llevar un registro de, por ejemplo, qu&eacute; rutas
+            has escalado, equipado, colaborado o guardado.
+          </span>
+        </li>
+        <li>
+          <span className="font-bold my-2">
+            Investigaci&oacute;n y desarrollo de productos:
+          </span>
+          <span>
+            &nbsp;
+            <br />
+            Usamos la informaci&oacute;n que tenemos para desarrollar, probar y
+            mejorar la aplicaci&oacute;n m&oacute;vil o iniciativas de la
+            Fundaci&oacute;n Andescalada.
+          </span>
+        </li>
+        <li>
+          <span className="font-bold my-2">
+            Ofrecemos mediciones, an&aacute;lisis y otros servicios comerciales:
+          </span>
+          <span>
+            &nbsp;
+            <br />
+            Usamos la informaci&oacute;n que tenemos para ayudar a socios
+            comerciales a medir la eficacia y distribuci&oacute;n de anuncios,
+            como productos recomendados, as&iacute; como a entender qu&eacute;
+            tipo de personas usan sus servicios y c&oacute;mo estas
+            interact&uacute;an con la aplicaci&oacute;n m&oacute;vil.
+          </span>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <span className="font-bold my-2">
+            Nos comunicamos contigo:
+            <br />
+          </span>
+          <span>
+            Utilizamos la informaci&oacute;n de contacto que tengamos en caso
+            que sea necesario ponernos en contacto contigo.
+          </span>
+        </li>
+      </ul>
+      <h2>
+        <span>
+          III. &iquest;C&oacute;mo se comparte esta informaci&oacute;n?{" "}
+        </span>
+      </h2>
+      <span>
+        No compartimos informaci&oacute;n personal. Solo compartimos
+        informaci&oacute;n agregada, y no desagregada, es decir no compartimos
+        informaci&oacute;n de un usuario en particular, sino de un grupo de
+        usuarios.
+      </span>
+      <p>
+        <span>
+          Tu informaci&oacute;n se comparte con otros de las siguientes formas:
+        </span>
+      </p>
+      <ul>
+        <li>
+          <span>En la aplicaci&oacute;n m&oacute;vil:</span>
+          <span>
+            &nbsp;Con otros usuarios de la aplicaci&oacute;n m&oacute;vil,
+            quienes sabr&aacute;n que has equipado o colaborado con una ruta de
+            escalada, adem&aacute;s de ver si eres parte de una coalici&oacute;n
+            o una iniciativa. Adem&aacute;s, cualquier usuario de la plataforma
+            tendr&aacute; acceso a tu perfil de usuario, que incluir&aacute; la
+            informaci&oacute;n que hayas proporcionado, como redes sociales,
+            descripci&oacute;n, ubicaci&oacute;n y rutas escaladas. Tu
+            n&uacute;mero de tel&eacute;fono no ser&aacute; visible.
+          </span>
+        </li>
+        <li>
+          <span>Con las coaliciones:</span>
+          <span>
+            &nbsp;En caso de que solicites unirte a una coalici&oacute;n o
+            iniciativa entregaremos tu nombre y n&uacute;mero de tel&eacute;fono
+            a los administradores de dicha coalici&oacute;n.
+          </span>
+        </li>
+        <li>
+          <span>
+            Con socios que usan nuestros servicios de an&aacute;lisis:{" "}
+          </span>
+          <span>
+            Proporcionamos estad&iacute;sticas y observaciones que ayudan a las
+            personas y a los negocios a entender a la comunidad escaladora,
+            siempre de manera an&oacute;nima. No vendemos tu informaci&oacute;n
+            a nadie, ni nunca lo haremos.
+          </span>
+        </li>
+        <li>
+          <span>Con investigadores y acad&eacute;micos: </span>
+          <span>
+            Brindamos informaci&oacute;n y contenido a socios investigadores y
+            acad&eacute;micos para que realicen investigaciones que permitan
+            profundizar los conocimientos y la innovaci&oacute;n que respalden
+            las iniciativas o misi&oacute;n de la Fundaci&oacute;n Andescalada,
+            y refuercen el descubrimiento y la innovaci&oacute;n sobre temas
+            relacionados con el bienestar social general y la escalada.
+          </span>
+        </li>
+        <li>
+          <span>
+            Con autoridades en base a una solicitud legal de informaci&oacute;n:
+          </span>
+          <span>&nbsp;Compartimos informaci&oacute;n </span>
+          <span>solo</span>
+          <span>
+            &nbsp;en respuesta a solicitudes legales. Siempre nos cercioramos
+            que la solicitud que se nos realice no infrinja{" "}
+          </span>
+          <span>
+            ninguna norma y, te notificaremos cuando tu informaci&oacute;n
+            personal sea solicitada, a menos que est&eacute; prohibido
+            legalmente hacerlo.
+          </span>
+        </li>
+      </ul>
+      <p>
+        <span></span>
+      </p>
+      <h2>
+        <span>I</span>
+        <span>
+          V. &iquest;C&oacute;mo puedo administrar o eliminar la
+          informaci&oacute;n sobre m&iacute;?
+        </span>
+      </h2>
+      <p>
+        <span>
+          Te damos la posibilidad de acceder a tus datos, editarlos y
+          suprimirlos desde el panel de administraci&oacute;n.
+        </span>
+      </p>
+      <p>
+        <span>
+          Almacenamos los datos hasta que se elimina tu cuenta. Cuando eliminas
+          tu cuenta, eliminamos el contenido que proporcionaste, como tus fotos,
+          informaci&oacute;n personal y rutas realizadas, y ya no podr&aacute;s
+          recuperar esa informaci&oacute;n despu&eacute;s.{" "}
+        </span>
+        <span>
+          No se eliminar&aacute; la informaci&oacute;n sobre ti que otros
+          compartieron, tal como si abriste una ruta
+        </span>
+        <span>
+          , porque no forma parte de tu cuenta. Para eliminar tu cuenta en
+          cualquier momento, ve a la configuraci&oacute;n de la
+          aplicaci&oacute;n m&oacute;vil Andescalada en tu perfil.
+        </span>
+      </p>
+      <h2>
+        <span></span>
+      </h2>
+      <h2>
+        <span>V</span>
+        <span>
+          . &iquest;C&oacute;mo notificaremos los cambios que se efect&uacute;en
+          en esta pol&iacute;tica?
+        </span>
+      </h2>
+      <p>
+        <span>
+          Te notificaremos en la aplicaci&oacute;n m&oacute;vil al realizar
+          cambios en esta pol&iacute;tica y te daremos la oportunidad de revisar
+          las modificaciones antes de seguir usandola.
+        </span>
+      </p>
+      <p>
+        <span></span>
+      </p>
+      <h2>
+        <span>VI</span>
+        <span>
+          . &iquest;C&oacute;mo puedes hacer llegar tus comentarios o dudas a
+          Andescalada?
+        </span>
+      </h2>
+      <p>
+        <span>Puedes contactarnos al correo </span>
+        <span>
+          <a href="mailto:contacto@andescalada.org">contacto@andescalada.org</a>
+        </span>
+      </p>
+      <p>
+        <span></span>
+      </p>{" "}
+    </section>
+  );
+};
+
+export default PrivacyPolicy;
