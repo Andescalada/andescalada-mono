1957d51e1b037c16981437b1c06033ea2a4a1ed6
elevyg <elevyg91@gmail.com>
2023-02-12 16:09:06 -0300

    refactor(app): Fallback Error

--- apps/expo/features/error/FallbackErrorScreen/FallbackErrorScreen.tsx
+++ apps/expo/features/error/FallbackErrorScreen/FallbackErrorScreen.tsx
@@ -1,12 +1,18 @@
-import { Box, Icon, Screen, Text } from "@andescalada/ui";
+import { Box, Icon, Ionicons, Pressable, Screen, Text } from "@andescalada/ui";
+import { useState } from "react";
+interface Props {
+  error: Error;
+  resetError: () => void;
+}
 
-const FallbackErrorScreen = () => {
+const FallbackErrorScreen = ({ error }: Props) => {
+  const [moreInfo, setMoreInfo] = useState(false);
   return (
     <Screen padding="m" backgroundColor="brand.primaryA">
       <Text variant="h1" color="brand.secondaryA">
         ¡Tuvimos un error!
       </Text>
-      <Box flex={1} marginTop="xl">
+      <Box marginVertical="xl">
         <Text variant="h4" marginTop="m" color="grayscale.white">
           Se nos cayó el ATC en la mitad de un multi-largo.
         </Text>
@@ -17,6 +23,28 @@ const FallbackErrorScreen = () => {
           Reinicia la app, si el error persiste bórrala y vuelve a descargarla.
         </Text>
       </Box>
+      <Pressable
+        backgroundColor="semantic.info"
+        borderRadius={16}
+        padding="m"
+        onPress={() => setMoreInfo((prev) => !prev)}
+      >
+        <Box
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Text>Más información</Text>
+          <Ionicons name="information-circle-sharp" size={20} />
+        </Box>
+        {moreInfo && (
+          <Box>
+            <Text variant="p2R" color="grayscale.white">
+              {error.message}
+            </Text>
+          </Box>
+        )}
+      </Pressable>
     </Screen>
   );
 };
