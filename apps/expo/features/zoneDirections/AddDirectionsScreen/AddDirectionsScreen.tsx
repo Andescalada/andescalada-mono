a43201574f70ba9424370f850e56f736810294d3
elevyg <elevyg91@gmail.com>
2023-02-13 16:11:32 -0300

    feat(app): transportation screen

--- apps/expo/features/zoneDirections/AddDirectionsScreen/AddDirectionsScreen.tsx
+++ apps/expo/features/zoneDirections/AddDirectionsScreen/AddDirectionsScreen.tsx
@@ -1,4 +1,16 @@
-import { Screen, Text } from "@andescalada/ui";
+import { TransportationModeSchema } from "@andescalada/db/zod";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  KeyboardAvoidingBox,
+  KeyboardDismiss,
+  Screen,
+  Text,
+  TextFieldAccordion,
+  TextInput,
+} from "@andescalada/ui";
+import transportationModeAssets from "@andescalada/utils/transportationModesAssets";
 import {
   ZoneDirectionsRoutes,
   ZoneDirectionsScreenProps,
@@ -9,8 +21,34 @@ type Props = ZoneDirectionsScreenProps<ZoneDirectionsRoutes.AddDirections>;
 
 const AddDirectionsScreen: FC<Props> = (props) => {
   return (
-    <Screen>
-      <Text></Text>
+    <Screen safeAreaDisabled padding="m">
+      <KeyboardAvoidingBox>
+        <KeyboardDismiss>
+          <Text variant="h4">Modo de transporte</Text>
+          <ButtonGroup value="" onChange={(value) => console.log(value)}>
+            <Box flexDirection="row" alignItems="stretch" flexWrap="wrap">
+              {TransportationModeSchema.options.map((mode) => (
+                <ButtonGroup.Item
+                  key={mode}
+                  label={transportationModeAssets[mode].label}
+                  value={mode}
+                />
+              ))}
+            </Box>
+          </ButtonGroup>
+          <Text variant="h4" marginTop="m">
+            Descripción
+          </Text>
+          <TextInput
+            containerProps={{ flex: 1, padding: "s" }}
+            multiline
+            textAlignVertical="top"
+            value={""}
+            onChangeText={() => console.log()}
+          />
+        </KeyboardDismiss>
+      </KeyboardAvoidingBox>
+      <Button variant="info" title="Continuar" marginVertical="l" />
     </Screen>
   );
 };
