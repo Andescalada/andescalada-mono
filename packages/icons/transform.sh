11e33b3a4881be58f1564bb9470cfb9d36eabb0f
elevyg <elevyg91@gmail.com>
2023-02-13 14:47:16 -0300

    feat(icons): destination icon

--- packages/icons/transform.sh
+++ packages/icons/transform.sh
@@ -5,21 +5,21 @@ npx svgo -f svg
 echo "Done cleaning :broom:"
 
 echo "Transforming icons"
-rm -r components
-mkdir components
-npx @svgr/cli --native --icon --no-prettier --filename-case kebab --replace-attr-values "#000={props.fill}" --out-dir components -- svg 
+rm -r components-native
+mkdir components-native
+npx @svgr/cli --native --icon --no-prettier --filename-case kebab --replace-attr-values "#000={props.fill}" --out-dir components-native -- svg 
 
 echo "Done transforming icons"
 
 rm map.ts
 
 printf "\e[1m\e[92mCreating the map...\e[0m\n"
-echo "const base = \"./components/\";" > map.ts
+echo "const base = \"./components-native/\";" > map.ts
 echo >> map.ts
 echo "export const Icons = {" >> map.ts
-for file in components/*.js
+for file in components-native/*.js
 do
-  if [ "$file" != "components/index.js" ]
+  if [ "$file" != "components-native/index.js" ]
   then
       base_name=$(basename ${file%.js})
       echo $base_name
