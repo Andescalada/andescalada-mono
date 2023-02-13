902b87b3c7a3766c3759debbad27c2c03b437667
elevyg <elevyg91@gmail.com>
2023-02-13 20:01:49 -0300

    feat(app): display zones directions

--- apps/expo/features/zoneDirections/ZoneDirectionsScreen/ZoneDirectionsScreen.tsx
+++ apps/expo/features/zoneDirections/ZoneDirectionsScreen/ZoneDirectionsScreen.tsx
@@ -1,4 +1,13 @@
-import { Box, Screen, Text, TextButton } from "@andescalada/ui";
+import {
+  ActivityIndicator,
+  AddButton,
+  Box,
+  Screen,
+  Text,
+  TextButton,
+} from "@andescalada/ui";
+import transportationModeAssets from "@andescalada/utils/transportationModesAssets";
+import { trpc } from "@andescalada/utils/trpc";
 import {
   ZoneDirectionsRoutes,
   ZoneDirectionsScreenProps,
@@ -16,11 +25,76 @@ const ZoneDirectionsScreen: FC<Props> = ({
   },
 }) => {
   const { permission } = usePermissions({ zoneId });
+  const { data, isLoading } = trpc.zones.directionsById.useQuery({ zoneId });
+
+  if (isLoading) {
+    return (
+      <Box flex={1} justifyContent="center" alignItems="center">
+        <ActivityIndicator size="large" />
+      </Box>
+    );
+  }
+
   return (
     <Screen safeAreaDisabled padding="m">
       <FlatList
-        data={[]}
-        renderItem={() => <Box />}
+        data={data?.ZoneDirections}
+        ListHeaderComponent={() => (
+          <Box>
+            <Box>
+              <Text variant="h1">{zoneName}</Text>
+            </Box>
+            {permission.has("Create") && (
+              <Box
+                justifyContent="space-between"
+                alignItems="center"
+                flexDirection="row"
+              >
+                <Text variant="p1B">Agregar direcciones</Text>
+                <AddButton />
+              </Box>
+            )}
+          </Box>
+        )}
+        renderItem={({ item }) => (
+          <Box
+            justifyContent="center"
+            flex={1}
+            borderRadius={30}
+            paddingVertical="m"
+          >
+            <Box flexDirection="row">
+              <Box
+                borderRadius={30}
+                backgroundColor="grayscale.600"
+                paddingHorizontal={"m"}
+                alignSelf="flex-start"
+              >
+                <Text variant="p1R">
+                  {transportationModeAssets[item.transportationMode].label}
+                </Text>
+              </Box>
+              {permission.has("Update") && (
+                <TextButton variant="info" marginHorizontal="m">
+                  Editar
+                </TextButton>
+              )}
+              {permission.has("Delete") && (
+                <TextButton variant="error">Borrar</TextButton>
+              )}
+            </Box>
+            <Box
+              padding="m"
+              backgroundColor="backgroundContrast"
+              borderRadius={15}
+              marginVertical="s"
+            >
+              <Text color="textContrast" variant="p2R">
+                {item.description?.originalText}
+              </Text>
+            </Box>
+          </Box>
+        )}
         ListEmptyComponent={() => (
           <Box marginTop="xxxl" alignItems="center">
             <Text variant="p1R">Sin direcciones</Text>
--- packages/api/src/routers/zones.ts
+++ packages/api/src/routers/zones.ts
@@ -370,6 +370,32 @@ export const zonesRouter = t.router({
 
       return parsedData;
     }),
+  directionsById: protectedZoneProcedure.query(async ({ ctx, input }) => {
+    const zone = await ctx.prisma.zone.findUnique({
+      where: { id: input.zoneId },
+      select: {
+        infoAccess: true,
+        ZoneDirections: {
+          orderBy: { createdAt: "desc" },
+          include: {
+            description: { select: { originalText: true } },
+            name: { select: { originalText: true } },
+          },
+        },
+      },
+    });
+    if (!ctx.permissions.has("Read") && zone?.infoAccess !== "Public") {
+      throw new TRPCError(
+        error.unauthorizedActionForZone(input.zoneId, "Read"),
+      );
+    }
+
+    if (!zone) {
+      throw new TRPCError(error.zoneNotFound(input.zoneId));
+    }
+
+    return zone;
+  }),
   addDirection: protectedZoneProcedure
     .input(zone.addDirections)
     .mutation(({ ctx, input }) => {
