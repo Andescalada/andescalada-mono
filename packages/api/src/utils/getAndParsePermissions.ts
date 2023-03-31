a6e05b95185abf1113e97ff03c3a6b8f249e3e50
elevyg <elevyg91@gmail.com>
2023-03-31 16:48:47 -0300

    chore: disable easlint warning

--- packages/api/src/utils/getAndParsePermissions.ts
+++ packages/api/src/utils/getAndParsePermissions.ts
@@ -5,6 +5,7 @@ const getAndParsePermissions = async (ctx: Context, zoneId: string) => {
   const permissions: Permissions = new Set();
 
   const roles = await ctx.prisma.roleByZone.findMany({
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     where: { User: { email: ctx.user!.email }, Zone: { id: zoneId } },
     select: {
       Role: {
