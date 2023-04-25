444a4d0aeba9b7b8166d42ae488db63e3caef453
elevyg <elevyg91@gmail.com>
2023-04-25 14:06:09 -0400

    fix(app): add scrollview to FallbackErrorScreen

--- apps/expo/features/error/FallbackErrorScreen/FallbackErrorScreen.tsx
+++ apps/expo/features/error/FallbackErrorScreen/FallbackErrorScreen.tsx
@@ -1,4 +1,12 @@
-import { Box, Icon, Ionicons, Pressable, Screen, Text } from "@andescalada/ui";
+import {
+  Box,
+  Icon,
+  Ionicons,
+  Pressable,
+  Screen,
+  ScrollView,
+  Text,
+} from "@andescalada/ui";
 import { FC, useState } from "react";
 interface Props {
   error?: Error;
@@ -8,43 +16,46 @@ interface Props {
 const FallbackErrorScreen = ({ error }: Props) => {
   const [moreInfo, setMoreInfo] = useState(false);
   return (
-    <Screen padding="m" backgroundColor="brand.primaryA">
-      <Text variant="h1" color="brand.secondaryA">
-        ¡Tuvimos un error!
-      </Text>
-      <Box marginVertical="xl">
-        <Text variant="h4" marginTop="m" color="grayscale.white">
-          Se nos cayó el ATC en la mitad de un multi-largo.
+    <Screen backgroundColor="brand.primaryA">
+      <ScrollView padding="m" nestedScrollEnabled>
+        <Text variant="h1" color="brand.secondaryA">
+          ¡Tuvimos un error!
         </Text>
-        <Box marginVertical="m" alignSelf="center">
-          <Icon name="climber-rappeling" size={100} />
-        </Box>
-        <Text variant="p1R" marginTop="m" color="grayscale.white">
-          Reinicia la app, si el error persiste bórrala y vuelve a descargarla.
-        </Text>
-      </Box>
-      <Pressable
-        backgroundColor="semantic.info"
-        borderRadius={16}
-        padding="m"
-        onPress={() => setMoreInfo((prev) => !prev)}
-      >
-        <Box
-          flexDirection="row"
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <Text>Más información</Text>
-          <Ionicons name="information-circle-sharp" size={20} />
-        </Box>
-        {moreInfo && (
-          <Box>
-            <Text variant="p2R" color="grayscale.white">
-              {JSON.stringify(error)}
-            </Text>
+        <Box marginVertical="xl">
+          <Text variant="h4" marginTop="m" color="grayscale.white">
+            Se nos cayó el ATC en la mitad de un multi-largo.
+          </Text>
+          <Box marginVertical="m" alignSelf="center">
+            <Icon name="climber-rappeling" size={100} />
           </Box>
-        )}
-      </Pressable>
+          <Text variant="p1R" marginTop="m" color="grayscale.white">
+            Reinicia la app, si el error persiste bórrala y vuelve a
+            descargarla.
+          </Text>
+        </Box>
+        <Pressable
+          backgroundColor="semantic.info"
+          borderRadius={16}
+          padding="m"
+          onPress={() => setMoreInfo((prev) => !prev)}
+        >
+          <Box
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Text>Más información</Text>
+            <Ionicons name="information-circle-sharp" size={20} />
+          </Box>
+          {moreInfo && (
+            <ScrollView>
+              <Text variant="p2R" color="grayscale.white">
+                {JSON.stringify(error)}
+              </Text>
+            </ScrollView>
+          )}
+        </Pressable>
+      </ScrollView>
     </Screen>
   );
 };
