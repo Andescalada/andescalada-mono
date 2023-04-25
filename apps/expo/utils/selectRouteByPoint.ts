1ddd0e2b69366e899ba8e49eae6799e0a8f8ec3c
elevyg <elevyg91@gmail.com>
2023-04-25 09:19:03 -0400

    fix(app): better point calculation

--- apps/expo/utils/selectRouteByPoint.ts
+++ apps/expo/utils/selectRouteByPoint.ts
@@ -5,14 +5,34 @@ const selectRouteByPoint = (
   routes: SkiaValue<{ pathToVector: SkPoint[]; routeId: string }[]>,
   point: SkPoint,
 ) => {
-  const selectedRoute = routes.current.find((route) => {
-    return pointInRoute({
-      path: route.pathToVector,
-      point,
-    });
-  });
+  const selectClosestRoute = routes.current.reduce<{
+    distances: { dx: number; dy: number };
+    routeId: string | undefined;
+  }>(
+    (prev, route) => {
+      const p = pointInRoute({
+        path: route.pathToVector,
+        point,
+        threshold: 1,
+      });
+      if (!p.isPointInRoute) {
+        return prev;
+      }
+      if (
+        abs(p.distances.dx, p.distances.dy) <
+        abs(prev.distances.dx, prev.distances.dy)
+      ) {
+        return { distances: p.distances, routeId: route.routeId };
+      }
+      return prev;
+    },
+    { distances: { dx: 1_000_000, dy: 1_000_000 }, routeId: undefined },
+  );
 
-  return selectedRoute?.routeId;
+  console.log(selectClosestRoute);
+  return selectClosestRoute?.routeId;
 };
 
 export default selectRouteByPoint;
+
+const abs = (x: number, y: number) => Math.abs(x) * Math.abs(y);
--- packages/climbs-drawer/pointInRoute.ts
+++ packages/climbs-drawer/pointInRoute.ts
@@ -16,6 +16,8 @@ const pointInRoute = ({
   const path = pathArg.map(roundPoint);
   const pt3 = roundPoint(point, roundPointDecimal);
 
+  const distances = { dx: 1000000, dy: 100000 };
+
   const isPointInRoute = path.some((pt1, index) => {
     if (index === pathLength - 1) return false;
     const pt2 = path[index + 1];
@@ -23,13 +25,16 @@ const pointInRoute = ({
     const dx = (pt3.x - pt1.x) / (pt2.x - pt1.x);
     const dy = (pt3.y - pt1.y) / (pt2.y - pt1.y);
 
+    distances.dx = dx;
+    distances.dy = dy;
+
     const betweenX = -threshold <= dx && dx <= threshold;
     const betweenY = -threshold <= dy && dy <= threshold;
 
     return betweenX && betweenY;
   });
 
-  return isPointInRoute;
+  return { isPointInRoute, distances };
 };
 
 export default pointInRoute;
