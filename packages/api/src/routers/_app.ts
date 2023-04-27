41cb2981f7760c888b4e586d60dc9fdd8e466380
elevyg <elevyg91@gmail.com>
2023-04-27 14:09:26 -0400

    chore(api): remove comments

--- packages/api/src/routers/_app.ts
+++ packages/api/src/routers/_app.ts
@@ -1,6 +1,3 @@
-/**
- * This file contains the root router of your tRPC-backend
- */
 import { agreementsRouter } from "@andescalada/api/src/routers/agreements";
 import { imagesRouter } from "@andescalada/api/src/routers/images";
 import { multiPitchRouter } from "@andescalada/api/src/routers/multiPitch";
@@ -18,12 +15,6 @@ import { zoneReviewRouter } from "@andescalada/api/src/routers/zoneReview";
 import { t } from "../createRouter";
 import { zonesRouter } from "./zones";
 
-/**
- * Create your application's root router
- * If you want to use SSG, you need export this
- * @link https://trpc.io/docs/ssg
- * @link https://trpc.io/docs/router
- */
 export const appRouter = t.router({
   zones: zonesRouter,
   sectors: sectorsRouter,
