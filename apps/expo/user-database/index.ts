0565861f95ee4926aba9e77bee66b936c0ef0473
elevyg <elevyg91@gmail.com>
2023-05-29 08:34:27 -0400

    feat: watermelon added to project

---
+++ apps/expo/user-database/index.ts
@@ -0,0 +1,27 @@
+import { Database } from "@nozbe/watermelondb";
+import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
+import { Platform } from "react-native";
+
+import migrations from "./model/migrations";
+import schema from "./model/schema";
+// import Post from './model/Post' // ⬅️ You'll import your Models here
+
+// First, create the adapter to the underlying database:
+const adapter = new SQLiteAdapter({
+  schema,
+  migrations,
+  dbName: "userAssets",
+  jsi: true,
+  // (optional, but you should implement this method)
+  onSetUpError: (error) => {
+    // Database failed to load -- offer the user to reload the app or log out
+  },
+});
+
+// Then, make a Watermelon database from it!
+const database = new Database({
+  adapter,
+  modelClasses: [
+    // Post, // ⬅️ You'll add Models to Watermelon here
+  ],
+});
