4bf8504a56746b4cb5ab16019d779191cd8b99e9
elevyg <elevyg91@gmail.com>
2023-05-31 14:37:32 -0400

    feat: uuid for watermelon ids

--- apps/expo/local-database/index.ts
+++ apps/expo/local-database/index.ts
@@ -1,5 +1,7 @@
 import { Database } from "@nozbe/watermelondb";
 import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
+import { setGenerator } from "@nozbe/watermelondb/utils/common/randomId";
+import { randomUUID } from "expo-crypto";
 
 import modelClasses from "./model";
 import migrations from "./model/migrations";
@@ -17,6 +19,8 @@ const adapter = new SQLiteAdapter({
     // Database failed to load -- offer the user to reload the app or log out
   },
 });
+
+setGenerator(randomUUID);
 
 // Then, make a Watermelon database from it!
 export const database = new Database({
