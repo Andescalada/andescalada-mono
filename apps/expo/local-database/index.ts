5a8f88fba1c1f12e1b68877f7b69ea366b807eb6
elevyg <elevyg91@gmail.com>
2023-06-01 11:37:13 -0400

    feat: clean db config

--- apps/expo/local-database/index.ts
+++ apps/expo/local-database/index.ts
@@ -2,27 +2,24 @@ import { Database } from "@nozbe/watermelondb";
 import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
 import { setGenerator } from "@nozbe/watermelondb/utils/common/randomId";
 import { randomUUID } from "expo-crypto";
+import * as Sentry from "sentry-expo";
 
 import modelClasses from "./model";
 import migrations from "./model/migrations";
 import schema from "./model/schema";
-// import Post from './model/Post' // ⬅️ You'll import your Models here
 
-// First, create the adapter to the underlying database:
 const adapter = new SQLiteAdapter({
   schema,
   // migrations,
   dbName: "userAssets",
   jsi: true,
-  // (optional, but you should implement this method)
   onSetUpError: (error) => {
-    // Database failed to load -- offer the user to reload the app or log out
+    Sentry.Native.captureException(error);
   },
 });
 
 setGenerator(randomUUID);
 
-// Then, make a Watermelon database from it!
 export const database = new Database({
   adapter,
   modelClasses,
