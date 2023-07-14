96314b20c250b63fe22e778b32ed39ed1dc2fe62
elevyg <elevyg91@gmail.com>
2023-07-14 18:13:05 -0400

    feat: create contest script

--- packages/migrator/src/create-contest.ts
+++ packages/migrator/src/create-contest.ts
@@ -3,22 +3,22 @@ import { PrismaClient } from "@andescalada/db";
 const db = new PrismaClient();
 
 export const createContest = async () => {
-  const starting = new Date();
-
   const chilcas = await db.zone.findFirst({
     where: { name: "Cuesta Las Chilcas" },
   });
   const chacabuco = await db.zone.findFirst({ where: { name: "Chacabuco" } });
   if (!chilcas) throw new Error("Zone Las Chilcas not found");
   if (!chacabuco) throw new Error("Zone Chacabuco not found");
-  const ending = new Date();
-  ending.setDate(starting.getDate() + 14);
-  const zones = await db.photoContest.create({
+
+  const starting = new Date(2023, 6, 14, 20, 0, 0); // '23:59:59 14-07-2023'
+  const ending = new Date(2023, 6, 31, 23, 59, 59); // '23:59:59 31-07-2023'
+
+  const contest = await db.photoContest.create({
     data: {
       starting,
       ending,
       Zones: { connect: [{ id: chilcas.id }, { id: chacabuco.id }] },
     },
   });
-  console.log("Zones created", zones);
+  console.log("Contest created successfully 🎉", contest);
 };
