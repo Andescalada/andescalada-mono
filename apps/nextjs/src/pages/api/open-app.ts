c5669eaef0863d14fe1ab7bf9b196fe2b2dfe539
elevyg <elevyg91@gmail.com>
2023-07-18 12:05:35 -0400

    feat(nextjs): open app

--- apps/nextjs/src/pages/api/open-app.ts
+++ apps/nextjs/src/pages/api/open-app.ts
@@ -3,25 +3,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 const OpenApp = (req: NextApiRequest, res: NextApiResponse) => {
   const userAgent = req.headers["user-agent"];
 
-  // Check if the user agent contains "Android" or "iOS" to determine the device type
   if (userAgent?.search("Android") !== -1) {
-    // Redirect Android users to a specific URL
-    res.redirect(
-      302,
-      "https://play.google.com/store/apps/details?id=com.andescalada.app&hl=es&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1",
-    );
+    res.status(200).json({
+      url: "https://play.google.com/store/apps/details?id=com.andescalada.app",
+    });
   } else if (
     userAgent?.search("iPhone") !== -1 ||
     userAgent?.search("iPad") !== -1
   ) {
-    // Redirect iOS users to a different URL
-    res.redirect(
-      302,
-      "https://apps.apple.com/us/app/andescalada/id6443438367?itsct=apps_box_badge&amp;itscg=30200",
-    );
+    res.status(200).json({
+      url: "https://apps.apple.com/us/app/andescalada/id6443438367",
+    });
   } else {
-    // Handle other cases or provide a default redirect URL
-    // res.redirect(301, "https://www.andescalada.org");
+    res.status(204);
   }
 };
 
--- apps/nextjs/src/pages/open-app.tsx
+++ apps/nextjs/src/pages/open-app.tsx
@@ -1,29 +1,29 @@
-import { useEffect, useState } from "react";
+import MobileVideoFrame from "components/MobileVideoFrame";
+import StoreBadges from "components/StoreBadges";
+import { useEffect } from "react";
+
+const handleRedirect = async () => {
+  const response = await fetch("/api/open-app");
+
+  if (response.ok) {
+    const { url } = await response.json();
+    window.location.href = url;
+  }
+};
 
 const OpenApp = () => {
-  const [redirecting, setRedirecting] = useState(false);
-
-  const handleRedirect = async () => {
-    setRedirecting(true);
-
-    try {
-      const response = await fetch("/api/open-app", { cache: "no-cache" });
-      if (response.ok) {
-        const { url } = await response.json();
-        window.location.href = url; // Perform the actual redirection
-      } else {
-        // Handle API errors if needed
-      }
-    } catch (error) {
-      // Handle fetch errors if needed
-    }
-  };
-
   useEffect(() => {
     handleRedirect();
   }, []);
 
-  return null;
+  return (
+    <section className="p-5 pt-10 md:p-20 flex flex-col items-center text-center gap-5 space-y-4 bg-black text-white">
+      <h1>Descarga la aplicación de Andescalada</h1>
+      <StoreBadges />
+      <MobileVideoFrame publicId="andescalada.org/draw-simple-route" />
+      <MobileVideoFrame publicId="andescalada.org/zone-privacy-options_msgaua" />
+    </section>
+  );
 };
 
 export default OpenApp;
