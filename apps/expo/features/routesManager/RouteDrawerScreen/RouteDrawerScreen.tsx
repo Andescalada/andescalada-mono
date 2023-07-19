3ea882705fe93849df98b0fcd2c51050f5ce20d4
elevyg <elevyg91@gmail.com>
2023-07-19 09:43:27 -0400

    fix(app): decoupling route drawer

--- apps/expo/features/routesManager/RouteDrawerScreen/RouteDrawerScreen.tsx
+++ apps/expo/features/routesManager/RouteDrawerScreen/RouteDrawerScreen.tsx
@@ -1,12 +1,12 @@
 import { ActivityIndicator, Screen } from "@andescalada/ui";
+import { trpc } from "@andescalada/utils/trpc";
 import {
   RoutesManagerNavigationRoutes,
   RoutesManagerScreenProps,
 } from "@features/routesManager/Navigation/types";
 import RouteDrawer from "@features/routesManager/RouteDrawerScreen/RouteDrawer";
 import parsedTopo from "@features/routesManager/utils/parsedTopos";
-import useToposById from "@hooks/offlineQueries/useToposById";
-import useTopoImage from "@hooks/useTopoImage";
+import { useGetTopoImage } from "@hooks/useTopoImage";
 import constants from "@utils/constants";
 import { FC, useMemo } from "react";
 
@@ -15,19 +15,18 @@ type Props =
 
 const DrawRoute: FC<Props> = ({
   route: {
-    params: { wallId, route: routeParams, topoId, zoneId },
+    params: { route: routeParams, topoId, zoneId },
   },
 }) => {
-  const { data } = useToposById({ topoId, zoneId }, {});
+  const { data } = trpc.topos.byId.useQuery({ topoId, zoneId });
 
   const topos = useMemo(
     () => parsedTopo(data, routeParams.id),
     [routeParams.id, data],
   );
 
-  const { fileUrl, isImageLoaded, fitted } = useTopoImage({
-    wallId,
-    zoneId,
+  const { fileUrl, isImageLoaded, fitted } = useGetTopoImage({
+    imageData: data?.image,
     imageQuality: constants.imageQuality,
   });
 
