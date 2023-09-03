cbad6b1c359b0d19d5a040d9cf977dec5c1dfdc7
elevyg <elevyg91@gmail.com>
2023-09-03 12:18:15 -0300

    chore: scripts

--- packages/console/src/current-verified.ts
+++ packages/console/src/current-verified.ts
@@ -3,31 +3,31 @@ import { PrismaClient } from "@andescalada/db";
 const db = new PrismaClient();
 
 const main = async () => {
-  // const verifiedTopos = await db.topo.findMany({
-  //   where: { UserPhotoContestTopo: { none: { id: { equals: undefined } } } },
-  //   select: { id: true },
-  // });
+  const verifiedTopos = await db.topo.findMany({
+    where: { UserPhotoContestTopo: { none: { id: { equals: undefined } } } },
+    select: { id: true },
+  });
 
   const toposFromContest = await db.topo.findMany({
     where: { UserPhotoContestTopo: { some: { id: { not: undefined } } } },
     select: { id: true },
   });
 
-  // for (const topo of verifiedTopos) {
-  //   console.log(`Adding verification to topo ${topo.id}`);
-  //   await db.topo.update({
-  //     where: { id: topo.id },
-  //     data: {
-  //       Verification: {
-  //         create: {
-  //           VerifierUser: { connect: { email: "elevy@andescalada.org" } },
-  //           status: "Approved",
-  //         },
-  //       },
-  //     },
-  //   });
-  //   console.log(`Verification added to topo ${topo.id}`);
-  // }
+  for (const topo of verifiedTopos) {
+    console.log(`Adding verification to topo ${topo.id}`);
+    await db.topo.update({
+      where: { id: topo.id },
+      data: {
+        Verification: {
+          create: {
+            VerifierUser: { connect: { email: "elevy@andescalada.org" } },
+            status: "Approved",
+          },
+        },
+      },
+    });
+    console.log(`Verification added to topo ${topo.id}`);
+  }
   console.log(`======================================`);
 
   for (const topo of toposFromContest) {
