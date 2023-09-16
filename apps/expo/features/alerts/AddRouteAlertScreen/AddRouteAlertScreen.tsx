8e668da07e1bb51cedd2b3ead67d2b34b07da800
elevyg <elevyg91@gmail.com>
2023-09-16 11:48:39 -0300

    feat(app): title and description

--- apps/expo/features/alerts/AddRouteAlertScreen/AddRouteAlertScreen.tsx
+++ apps/expo/features/alerts/AddRouteAlertScreen/AddRouteAlertScreen.tsx
@@ -2,7 +2,14 @@ import {
   RouteAlertKindSchema,
   RouteAlertSeveritySchema,
 } from "@andescalada/db/zod";
-import { Box, ButtonGroup, Screen, Text } from "@andescalada/ui";
+import {
+  Box,
+  ButtonGroup,
+  Screen,
+  ScrollView,
+  Text,
+  TextInput,
+} from "@andescalada/ui";
 import {
   AlertsRoutes,
   AlertsScreenProps,
@@ -40,45 +47,59 @@ const AddRouteAlertScreen: FC<Props> = (props) => {
   }, []);
 
   return (
-    <Screen padding="m">
-      <SelectImage
-        image={imageToDisplay}
-        isLoading={loadingUpload}
-        onPickImage={pickImageHandler}
-        onDeletePickedImage={() => setImageToDisplay(null)}
-      />
-      <ButtonGroup
-        value={alertKind}
-        onChange={(v) => setAlertKind(v as typeof RouteAlertKindSchema._type)}
-      >
-        <Box flexWrap="wrap" flexDirection="row" gap="m">
-          {RouteAlertKindSchema.options.map((kind) => (
-            <ButtonGroup.Item
-              label={routeAlertKind(kind).label}
-              value={kind}
-              key={kind}
-            />
-          ))}
+    <Screen>
+      <ScrollView showsVerticalScrollIndicator={false} padding="m">
+        <SelectImage
+          image={imageToDisplay}
+          isLoading={loadingUpload}
+          onPickImage={pickImageHandler}
+          onDeletePickedImage={() => setImageToDisplay(null)}
+        />
+        <Box width="100%" flex={1} gap="m">
+          <TextInput
+            placeholder="Titulo"
+            containerProps={{ height: 50, paddingLeft: "s" }}
+          />
+          <TextInput
+            multiline
+            placeholder={`Escribe una descripción de la alerta`}
+            textAlignVertical="top"
+            containerProps={{ flex: 1, height: 200, padding: "s" }}
+          />
         </Box>
-      </ButtonGroup>
-      <ButtonGroup
-        value={severity}
-        onChange={(v) =>
-          setSeverity(v as typeof RouteAlertSeveritySchema._type)
-        }
-      >
-        <Box flexWrap="wrap" flexDirection="row" gap="m">
-          {RouteAlertSeveritySchema.options.map((kind) => (
-            <ButtonGroup.Item
-              label={routeAlertSeverity(kind).label}
-              backgroundColor={routeAlertSeverity(kind).backgroundColor}
-              textColor={routeAlertSeverity(kind).color}
-              value={kind}
-              key={kind}
-            />
-          ))}
-        </Box>
-      </ButtonGroup>
+        <ButtonGroup
+          value={alertKind}
+          onChange={(v) => setAlertKind(v as typeof RouteAlertKindSchema._type)}
+        >
+          <Box flexWrap="wrap" flexDirection="row" gap="m">
+            {RouteAlertKindSchema.options.map((kind) => (
+              <ButtonGroup.Item
+                label={routeAlertKind(kind).label}
+                value={kind}
+                key={kind}
+              />
+            ))}
+          </Box>
+        </ButtonGroup>
+        <ButtonGroup
+          value={severity}
+          onChange={(v) =>
+            setSeverity(v as typeof RouteAlertSeveritySchema._type)
+          }
+        >
+          <Box flexWrap="wrap" flexDirection="row" gap="m">
+            {RouteAlertSeveritySchema.options.map((kind) => (
+              <ButtonGroup.Item
+                label={routeAlertSeverity(kind).label}
+                backgroundColor={routeAlertSeverity(kind).backgroundColor}
+                textColor={routeAlertSeverity(kind).color}
+                value={kind}
+                key={kind}
+              />
+            ))}
+          </Box>
+        </ButtonGroup>
+      </ScrollView>
     </Screen>
   );
 };
