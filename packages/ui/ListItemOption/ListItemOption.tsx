87a3b5623358751bef8f30cfed348f6b4aaa106c
elevyg <elevyg91@gmail.com>
2023-09-24 12:39:05 -0300

    feat(ui): ListItemOption with icon

--- packages/ui/ListItemOption/ListItemOption.tsx
+++ packages/ui/ListItemOption/ListItemOption.tsx
@@ -1,15 +1,37 @@
 import { ComponentProps, ReactNode } from "react";
 
+import Box from "../Box/Box";
+import Ionicons, { IoniconsNames } from "../Ionicons/Ionicons";
 import ListItem from "../ListItem/ListItem";
 import Text from "../Text/Text";
 
 interface Props extends ComponentProps<typeof ListItem> {
   children: ReactNode;
+  icon?: IoniconsNames;
+  iconSize?: ComponentProps<typeof Ionicons>["size"];
+  iconProps?: ComponentProps<typeof Ionicons>;
 }
 
-const ListItemOption = ({ children, ...props }: Props) => {
+const ListItemOption = ({
+  children,
+  icon,
+  iconProps,
+  iconSize = 20,
+  ...props
+}: Props) => {
   return (
-    <ListItem marginVertical="s" {...props}>
+    <ListItem
+      flexDirection="row"
+      alignItems="center"
+      marginVertical="s"
+      gap="s"
+      {...props}
+    >
+      {icon && (
+        <Box justifyContent="center">
+          <Ionicons {...iconProps} name={icon} size={iconSize} />
+        </Box>
+      )}
       <Text variant="p2R">{children}</Text>
     </ListItem>
   );
