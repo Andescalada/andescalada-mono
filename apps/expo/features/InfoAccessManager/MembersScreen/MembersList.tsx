ec6f33e035d1ecc00905d9a85107014486aac7f0
elevyg <elevyg91@gmail.com>
2023-09-27 09:20:59 -0300

    feat(app): invite in members tab

--- apps/expo/features/InfoAccessManager/MembersScreen/MembersList.tsx
+++ apps/expo/features/InfoAccessManager/MembersScreen/MembersList.tsx
@@ -1,13 +1,26 @@
 import type { Zone } from "@andescalada/db";
-import { RoleNamesSchema } from "@andescalada/db/zod";
-import { ActivityIndicator, Box, Button, Text } from "@andescalada/ui";
+import { RoleNamesSchema, StatusSchema } from "@andescalada/db/zod";
+import {
+  ActivityIndicator,
+  AddButton,
+  Box,
+  Button,
+  Pressable,
+  Text,
+} from "@andescalada/ui";
 import { trpc } from "@andescalada/utils/trpc";
 import MemberModal from "@features/InfoAccessManager/MembersScreen/MemberModal";
 import UserItem from "@features/InfoAccessManager/MembersScreen/UserItem";
+import {
+  InfoAccessManagerNavigationProps,
+  InfoAccessManagerRoutes,
+} from "@features/InfoAccessManager/Navigation/types";
+import useZonesAllSectors from "@hooks/offlineQueries/useZonesAllSectors";
 import usePauseAccess from "@hooks/usePauseAccess";
 import usePermissions from "@hooks/usePermissions";
 import useRefresh from "@hooks/useRefresh";
-import { FC, useMemo, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { FC, useCallback, useMemo, useState } from "react";
 import { Alert, FlatList } from "react-native";
 
 interface Props {
@@ -47,6 +60,24 @@ const MembersList: FC<Props> = ({ zoneId }) => {
 
   const { permission } = usePermissions({ zoneId });
 
+  const zone = useZonesAllSectors({ zoneId });
+
+  const navigation =
+    useNavigation<
+      InfoAccessManagerNavigationProps<InfoAccessManagerRoutes.MembersScreen>
+    >();
+
+  const onInvite = useCallback(() => {
+    if (zone.data?.currentStatus !== StatusSchema.enum.Published) {
+      Alert.alert("Solo puedes invitar usuarios cuando la zona esté publicada");
+      return;
+    }
+    navigation.navigate(InfoAccessManagerRoutes.InviteUserToZoneScreen, {
+      zoneId,
+      zoneName: zone.data?.name,
+    });
+  }, [zone.data?.currentStatus, navigation, zoneId, zone.data?.name]);
+
   if (isLoading)
     return (
       <Box flex={1} justifyContent="center" alignItems="center">
@@ -61,7 +92,17 @@ const MembersList: FC<Props> = ({ zoneId }) => {
         onDismiss={() => setSelectedUserId(null)}
         navigateToUser={(userId) => setSelectedUserId(userId)}
       />
-
+      {permission?.has("GrantAccess") && (
+        <Pressable
+          flexDirection="row"
+          justifyContent="space-between"
+          margin="m"
+          onPress={onInvite}
+        >
+          <Text variant="h4">Invitar</Text>
+          <AddButton onPress={onInvite} />
+        </Pressable>
+      )}
       <FlatList
         data={data}
         keyExtractor={(item) => item.id}
