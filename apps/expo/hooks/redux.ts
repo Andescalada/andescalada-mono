a0e35c2fabed42d90fcad87a58a1b9b2446c81c7
elevyg <elevyg91@gmail.com>
2024-08-26 14:25:18 -0400

    chore: remove redux

--- apps/expo/hooks/redux.ts
+++
@@ -1,5 +0,0 @@
-import type { AppDispatch, RootState } from "@store/index";
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
