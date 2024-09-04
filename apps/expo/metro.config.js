d486c299d68e9fb6ea1c9bf3ec62ea91a7ba1ef1
elevyg <elevyg91@gmail.com>
2024-09-04 11:15:48 -0400

    pnpm still not working

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { getDefaultConfig } = require("expo/metro-config");
+const { FileStore } = require("metro-cache");
 const path = require("path");
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
 
 // Find the project and workspace directories
 const projectRoot = __dirname;
-// This can be replaced with `find-yarn-workspace-root`
+
 const workspaceRoot = path.resolve(projectRoot, "../..");
 
 const config = getDefaultConfig(projectRoot);
@@ -17,14 +19,13 @@ config.resolver.nodeModulesPaths = [
   path.resolve(workspaceRoot, "node_modules"),
 ];
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
+// config.resolver.disableHierarchicalLookup = true;
 
-module.exports = () => {
-  return {
-    ...config,
-    resolver: {
-      ...config.resolver,
-      sourceExts: [...config.resolver.sourceExts, "cjs"],
-    },
-  };
-};
+// Use turborepo to restore the cache when possible
+config.cacheStores = [
+  new FileStore({
+    root: path.join(projectRoot, "node_modules", ".cache", "metro"),
+  }),
+];
+
+module.exports = makeMetroConfig(config);
